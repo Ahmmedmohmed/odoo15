cf00bea9893bd781d5491574f5498e47bf05613b
Ahmmedmohmed <am2157678@gmail.com>
2025-12-31 00:07:17 +0200

    u[

--- nile_air_appraisal/models/res_user.py
+++ nile_air_appraisal/models/res_user.py
@@ -3,15 +3,13 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    # تحديد رتبة المستخدم في نظام التقييم
     appraisal_role = fields.Selection(
-        string="Appraisal Role",
         selection=[
-            ('user', 'User / Employee'),
+            ('user', 'Employee'),
             ('manager', 'Direct Manager'),
             ('hr', 'HR Manager'),
             ('co', 'CEO / CO'),
         ],
-        default='user',
-        help="Determines the access level in the Appraisal workflow."
+        string="Appraisal Role",
+        default='user'
     )
