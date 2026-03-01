ca8fd9a6f45ea19a3ef5d32c2056b9b90e046a04
Ahmmedmohmed <am2157678@gmail.com>
2026-03-01 23:24:58 +0200

    up

--- simplify_access_management/controllers/export.py
+++ simplify_access_management/controllers/export.py
@@ -8,12 +8,11 @@ from odoo.addons.web.controllers.main import Export
 
 class Export(Export):
 
-    # في أودو 15، لازم تكرر الـ route هنا بالظبط عشان يتجنب خطأ original_func
-    # وبنفس الإعدادات الأصلية للسيرفر
+    # في أودو 15، الدالة الافتراضية في الكنترولر اسمها get_fields وليست fields_get
     @http.route('/web/export/get_fields', type='json', auth="user")
-    def fields_get(self, model):
+    def get_fields(self, model, import_compat=True, **kw):
         # استدعاء الدالة من الكلاس الأب
-        fields = super(Export, self).fields_get(model)
+        fields = super(Export, self).get_fields(model, import_compat=import_compat, **kw)
 
         # منطق إخفاء الحقول الخاص بك
         invisible_field_ids = request.env['hide.field'].sudo().search([
@@ -30,8 +29,11 @@ class Export(Export):
         hidden_field_names = invisible_field_ids.mapped('field_id.name')
 
         # تنظيف القائمة المرجعة
-        for field_name in list(fields.keys()):
-            if field_name in hidden_field_names and field_name != "id":
-                del fields[field_name]
+        # الدالة الأصلية في أودو ترجع قائمة من القواميس وليس قاموساً
+        filtered_fields = []
+        for field in fields:
+            field_name = field.get('id')
+            if field_name not in hidden_field_names or field_name == "id":
+                filtered_fields.append(field)
 
-        return fields
+        return filtered_fields
