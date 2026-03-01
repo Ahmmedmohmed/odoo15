cec3d374c65dd2bfe5ba52737c49d429b10a0ac7
Ahmmedmohmed <am2157678@gmail.com>
2026-03-01 23:14:53 +0200

    up

--- simplify_access_management/controllers/export.py
+++ simplify_access_management/controllers/export.py
@@ -2,19 +2,20 @@
 
 from odoo import http
 from odoo.http import request
-# الاستيراد الصحيح من main لنسخة 15
+# الاستيراد لازم يكون من المسار الرئيسي لويب في نسخة 15
 from odoo.addons.web.controllers.main import Export
 
 
 class Export(Export):
 
-    # في Odoo 15، لازم تكرر الـ route هنا عشان محرك الروابط ميتلخبطش
+    # في أودو 15، لازم تكرر الـ route هنا بالظبط عشان يتجنب خطأ original_func
+    # وبنفس الإعدادات الأصلية للسيرفر
     @http.route('/web/export/get_fields', type='json', auth="user")
     def fields_get(self, model):
-        # استدعاء الدالة الأصلية
+        # استدعاء الدالة من الكلاس الأب
         fields = super(Export, self).fields_get(model)
 
-        # منطق إخفاء الحقول بناءً على صلاحيات Access Management
+        # منطق إخفاء الحقول الخاص بك
         invisible_field_ids = request.env['hide.field'].sudo().search([
             ('access_management_id.company_ids', 'in', request.env.company.id),
             ('model_id.model', '=', model),
@@ -26,10 +27,9 @@ class Export(Export):
         if not invisible_field_ids:
             return fields
 
-        # استخراج أسماء الحقول المخفية
         hidden_field_names = invisible_field_ids.mapped('field_id.name')
 
-        # تنظيف القائمة المرجعة للمتصفح
+        # تنظيف القائمة المرجعة
         for field_name in list(fields.keys()):
             if field_name in hidden_field_names and field_name != "id":
                 del fields[field_name]
