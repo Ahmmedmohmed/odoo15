57c4a94c8fa79aa90cf69ea3fb0ca0268c126328
Ahmmedmohmed <am2157678@gmail.com>
2026-03-01 23:10:10 +0200

    up

--- simplify_access_management/controllers/export.py
+++ simplify_access_management/controllers/export.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from odoo import http
-from odoo.exceptions import UserError
-# التعديل هنا: استيراد Export من main بدلاً من export
-from odoo.addons.web.controllers.main import Export
 from odoo.http import request
+# استيراد الكلاس الأصلي من main
+from odoo.addons.web.controllers.main import Export
 
 
 class Export(Export):
 
+    # لازم تعيد تعريف الـ route بالكامل هنا عشان Odoo 15 يقدر يربطها صح
     @http.route('/web/export/get_fields', type='json', auth="user")
     def fields_get(self, model):
-        # تنفيذ الدالة الأصلية لجلب الحقول
+        # استدعاء الدالة الأصلية باستخدام super
         fields = super(Export, self).fields_get(model)
 
-        # جلب الحقول المحظورة لهذا المستخدم والشركة الحالية
-        # ملاحظة: تم إضافة .sudo() لضمان جلب قواعد البيانات حتى لو المستخدم صلاحياته محدودة
+        # منطق إخفاء الحقول
         invisible_field_ids = request.env['hide.field'].sudo().search([
             ('access_management_id.company_ids', 'in', request.env.company.id),
             ('model_id.model', '=', model),
@@ -27,10 +26,9 @@ class Export(Export):
         if not invisible_field_ids:
             return fields
 
-        # استخراج أسماء الحقول المخفية لتقليل عدد اللفات داخل اللوب
         hidden_field_names = invisible_field_ids.mapped('field_id.name')
 
-        # تنظيف قائمة الحقول المتاحة للتصدير
+        # تنظيف القائمة
         for field_name in list(fields.keys()):
             if field_name in hidden_field_names and field_name != "id":
                 del fields[field_name]
