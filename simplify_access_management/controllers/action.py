29d46c3b2cf6c6944ae6671d6aa0c005b88e79c4
Ahmmedmohmed <am2157678@gmail.com>
2026-03-01 23:12:41 +0200

    up

--- simplify_access_management/controllers/action.py
+++ simplify_access_management/controllers/action.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# التعديل الجوهري: استيراد ensure_db و Action و Home من المسار الصحيح لأودو 15
+# الاستيراد الصحيح والمجمع من main لنسخة 15
 from odoo.addons.web.controllers.main import Home, Action, ensure_db
 from odoo.tools.translate import _
 from odoo.http import request
@@ -10,27 +10,27 @@ from odoo import http
 
 class Action(Action):
 
+    # في Odoo 15، يجب تكرار الـ route بالكامل عند عمل Override
     @http.route('/web/action/run', type='json', auth="user")
     def run(self, action_id, context=None):
-        # تنفيذ الدالة الأصلية
         res = super(Action, self).run(action_id, context)
 
-        if res and isinstance(res, dict):
-            # البحث عن إعدادات الوصول للمستخدم والشركة الحالية
-            cids = request.httprequest.cookies.get('cids') and request.httprequest.cookies.get('cids').split(',')[
-                0] or request.env.company.id
+        if isinstance(res, dict) and res.get('res_model'):
+            # جلب معرف الشركة من الكوكيز أو البيئة الحالية
+            cids = request.httprequest.cookies.get('cids')
+            company_id = int(cids.split(',')[0]) if cids else request.env.company.id
 
             remove_actions = request.env['remove.action'].sudo().search([
-                ('access_management_id.company_ids', 'in', int(cids)),
+                ('access_management_id.company_ids', 'in', company_id),
                 ('access_management_id.active', '=', True),
                 ('access_management_id.user_ids', 'in', request.env.user.id),
                 ('model_id.model', '=', res.get('res_model'))
             ])
 
             if remove_actions:
-                # حذف أنواع الـ Views المحظورة (مثل القائمة أو التقويم) من الاستجابة
                 forbidden_views = remove_actions.mapped('view_data_ids.techname')
-                if 'views' in res:
+                if res.get('views'):
+                    # تصفية الواجهات (Views) بأسلوب بايثون السريع
                     res['views'] = [v for v in res['views'] if v[1] not in forbidden_views]
 
         return res
@@ -39,13 +39,13 @@ class Action(Action):
     def load(self, action_id, additional_context=None):
         res = super(Action, self).load(action_id, additional_context=additional_context)
 
-        if res and isinstance(res, dict):
-            cids = request.httprequest.cookies.get('cids') and request.httprequest.cookies.get('cids').split(',')[
-                0] or request.env.company.id
+        if isinstance(res, dict) and res.get('res_model'):
+            cids = request.httprequest.cookies.get('cids')
+            company_id = int(cids.split(',')[0]) if cids else request.env.company.id
 
             remove_actions = request.env['remove.action'].sudo().search([
                 ('view_data_ids', '!=', False),
-                ('access_management_id.company_ids', 'in', int(cids)),
+                ('access_management_id.company_ids', 'in', company_id),
                 ('access_management_id.active', '=', True),
                 ('access_management_id.user_ids', 'in', request.env.user.id),
                 ('model_id.model', '=', res.get('res_model'))
@@ -53,13 +53,12 @@ class Action(Action):
 
             if remove_actions:
                 forbidden_views = remove_actions.mapped('view_data_ids.techname')
-                if 'views' in res:
+                if res.get('views'):
                     res['views'] = [v for v in res['views'] if v[1] not in forbidden_views]
 
-            # منع الدخول إذا لم يتبق أي واجهة عرض مسموحة
+            # التحقق من وجود واجهات متبقية
             if 'views' in res and not res.get('views'):
-                raise UserError(
-                    _("You don't have the permission to access any views. Please contact to administrator."))
+                raise UserError(_("You don't have permission to access any views. Please contact your administrator."))
 
         return res
 
@@ -70,31 +69,31 @@ class Home(Home):
     def web_client(self, s_action=None, **kw):
         ensure_db()
 
-        # Odoo 15 Cache Clearing
-        request.env.registry.clear_all_caches()
+        # تنظيف الكاش لضمان تطبيق الصلاحيات الجديدة فوراً
+        if request.session.uid:
+            request.env.registry.clear_all_caches()
 
-        user = request.env.user.browse(request.session.uid)
+        # التحقق من وجود مستخدم مسجل
+        if request.session.uid:
+            user = request.env.user.browse(request.session.uid)
 
-        # منطق تعطيل الـ Debug Mode بناءً على صلاحيات المستخدم
-        if not kw.get('debug') or kw.get('debug') != "0":
-            cids = request.httprequest.cookies.get('cids') and request.httprequest.cookies.get('cids').split(',')[
-                0] or request.env.company.id
+            # منع وضع المطور (Debug Mode)
+            if kw.get('debug') and kw.get('debug') != "0":
+                cids = request.httprequest.cookies.get('cids')
+                try:
+                    company_id = int(cids.split(',')[0]) if cids else request.env.company.id
+                except:
+                    company_id = request.env.company.id
 
-            # التأكد من تحويل cids لرقم صحيح
-            try:
-                company_id = int(cids)
-            except:
-                company_id = request.env.company.id
+                access_management = request.env['access.management'].sudo().search([
+                    ('active', '=', True),
+                    ('company_ids', 'in', company_id),
+                    ('disable_debug_mode', '=', True),
+                    ('user_ids', 'in', user.id)
+                ], limit=1)
 
-            access_management = request.env['access.management'].sudo().search([
-                ('active', '=', True),
-                ('company_ids', 'in', company_id),
-                ('disable_debug_mode', '=', True),
-                ('user_ids', 'in', user.id)
-            ], limit=1)
-
-            if access_management.id:
-                # إعادة التوجيه لتعطيل وضع المطور
-                return request.redirect('/web?debug=0')
+                if access_management:
+                    # إعادة التوجيه لتعطيل الـ Debug
+                    return request.redirect('/web?debug=0')
 
         return super(Home, self).web_client(s_action=s_action, **kw)
--- simplify_access_management/controllers/export.py
+++ simplify_access_management/controllers/export.py
@@ -2,19 +2,19 @@
 
 from odoo import http
 from odoo.http import request
-# استيراد الكلاس الأصلي من main لضمان الوصول لكل الأدوات
+# الاستيراد الصحيح لأودو 15
 from odoo.addons.web.controllers.main import Export
 
 
 class Export(Export):
 
-    # لازم نكرر الـ route هنا بالظبط زي ما هو موجود في سورس أودو 15
+    # في أودو 15، لازم تكرر الـ route هنا بالظبط عشان يتجنب خطأ original_func
     @http.route('/web/export/get_fields', type='json', auth="user")
     def fields_get(self, model):
         # استدعاء الدالة الأصلية
         fields = super(Export, self).fields_get(model)
 
-        # منطق الحماية وإخفاء الحقول
+        # جلب الحقول المخفية بناءً على صلاحيات الوصول
         invisible_field_ids = request.env['hide.field'].sudo().search([
             ('access_management_id.company_ids', 'in', request.env.company.id),
             ('model_id.model', '=', model),
@@ -29,7 +29,7 @@ class Export(Export):
         # استخراج أسماء الحقول المخفية
         hidden_field_names = invisible_field_ids.mapped('field_id.name')
 
-        # حذف الحقول من القائمة المرجعة للمتصفح
+        # حذف الحقول من النتيجة النهائية
         for field_name in list(fields.keys()):
             if field_name in hidden_field_names and field_name != "id":
                 del fields[field_name]
