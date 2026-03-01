582a3c7f80b961fd125f82cd688e4d9b8a05e8cf
Ahmmedmohmed <am2157678@gmail.com>
2026-03-01 23:29:15 +0200

    up

--- simplify_access_management/__init__.py
+++ simplify_access_management/__init__.py
@@ -1,19 +1,30 @@
+# -*- coding: utf-8 -*-
+
 from . import models
 # from . import wizard
 from . import controllers
 
-
 from odoo import api, SUPERUSER_ID
 
 
-def uninstall_hook(env):
-    env['ir.config_parameter'].search([('key', '=', 'uninstall_check')]).unlink()
+# تعديل uninstall_hook لاستقبال cr و registry
+def uninstall_hook(cr, registry):
+    # إنشاء env يدويًا في Odoo 15
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['ir.config_parameter'].sudo().search([('key', '=', 'uninstall_check')]).unlink()
 
 
-def post_install_action_dup_hook(env):
+# تعديل post_install_action_dup_hook لاستقبال cr و registry
+def post_install_action_dup_hook(cr, registry):
+    # إنشاء env يدويًا في Odoo 15 لضمان عمل الكود البرمجي بالأسفل
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
     action_data_obj = env['action.data']
     menu_item_obj = env['menu.item']
-    for action in env['ir.actions.actions'].search([]):
+
+    # تنفيذ منطق البحث والإنشاء
+    for action in env['ir.actions.actions'].sudo().search([]):
         action_data_obj.create({'name': action.name, 'action_id': action.id})
-    for menu in env['ir.ui.menu'].search([]):
+
+    for menu in env['ir.ui.menu'].sudo().search([]):
         menu_item_obj.create({'name': menu.display_name, 'menu_id': menu.id})
