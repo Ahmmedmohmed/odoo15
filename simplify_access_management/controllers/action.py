6605911c8390c27955111ef154a32c14f464e88a
Ahmmedmohmed <am2157678@gmail.com>
2026-03-24 15:23:31 +0200

    up

--- simplify_access_management/controllers/action.py
+++ simplify_access_management/controllers/action.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# الاستيراد الصحيح والمجمع من main لنسخة 15
 from odoo.addons.web.controllers.main import Home, Action, ensure_db
 from odoo.tools.translate import _
 from odoo.http import request
-from odoo.exceptions import UserError
 from odoo import http
 
 
 class Action(Action):
 
-    # في Odoo 15، يجب تكرار الـ route بالكامل عند عمل Override
+    # التعديل الجوهري: إضافة **kwargs لاستقبال أي متغيرات إضافية يرسلها أودو 15
     @http.route('/web/action/run', type='json', auth="user")
-    def run(self, action_id, context=None):
-        res = super(Action, self).run(action_id, context)
+    def run(self, action_id, context=None, **kwargs):
+        # تمرير كل المتغيرات للـ super لضمان عدم حدوث TypeError
+        res = super(Action, self).run(action_id, context=context, **kwargs)
 
         if isinstance(res, dict) and res.get('res_model'):
-            # جلب معرف الشركة من الكوكيز أو البيئة الحالية
             cids = request.httprequest.cookies.get('cids')
-            company_id = int(cids.split(',')[0]) if cids else request.env.company.id
+            try:
+                company_id = int(cids.split(',')[0]) if cids else request.env.company.id
+            except:
+                company_id = request.env.company.id
 
+            # البحث في موديل الصلاحيات (Sudo لضمان القراءة)
             remove_actions = request.env['remove.action'].sudo().search([
                 ('access_management_id.company_ids', 'in', company_id),
                 ('access_management_id.active', '=', True),
@@ -30,21 +32,24 @@ class Action(Action):
             if remove_actions:
                 forbidden_views = remove_actions.mapped('view_data_ids.techname')
                 if res.get('views'):
-                    # تصفية الواجهات (Views) بأسلوب بايثون السريع
+                    # تصفية الواجهات (Views) التي ليس للمستخدم صلاحية عليها
                     res['views'] = [v for v in res['views'] if v[1] not in forbidden_views]
 
         return res
 
     @http.route('/web/action/load', type='json', auth="user")
-    def load(self, action_id, additional_context=None):
-        res = super(Action, self).load(action_id, additional_context=additional_context)
+    def load(self, action_id, additional_context=None, **kwargs):
+        # إضافة **kwargs هنا أيضاً لضمان التوافق مع نداءات الـ RPC في V15
+        res = super(Action, self).load(action_id, additional_context=additional_context, **kwargs)
 
         if isinstance(res, dict) and res.get('res_model'):
             cids = request.httprequest.cookies.get('cids')
-            company_id = int(cids.split(',')[0]) if cids else request.env.company.id
+            try:
+                company_id = int(cids.split(',')[0]) if cids else request.env.company.id
+            except:
+                company_id = request.env.company.id
 
             remove_actions = request.env['remove.action'].sudo().search([
-                ('view_data_ids', '!=', False),
                 ('access_management_id.company_ids', 'in', company_id),
                 ('access_management_id.active', '=', True),
                 ('access_management_id.user_ids', 'in', request.env.user.id),
@@ -55,10 +60,6 @@ class Action(Action):
                 forbidden_views = remove_actions.mapped('view_data_ids.techname')
                 if res.get('views'):
                     res['views'] = [v for v in res['views'] if v[1] not in forbidden_views]
-
-            # التحقق من وجود واجهات متبقية
-            if 'views' in res and not res.get('views'):
-                raise UserError(_("You don't have permission to access any views. Please contact your administrator."))
 
         return res
 
@@ -69,16 +70,12 @@ class Home(Home):
     def web_client(self, s_action=None, **kw):
         ensure_db()
 
-        # تنظيف الكاش لضمان تطبيق الصلاحيات الجديدة فوراً
+        # التأكد من هوية المستخدم قبل فحص صلاحيات الـ Debug Mode
         if request.session.uid:
-            request.env.registry.clear_caches()
+            user = request.env['res.users'].sudo().browse(request.session.uid)
 
-        # التحقق من وجود مستخدم مسجل
-        if request.session.uid:
-            user = request.env.user.browse(request.session.uid)
-
-            # منع وضع المطور (Debug Mode)
-            if kw.get('debug') and kw.get('debug') != "0":
+            # إذا حاول المستخدم تفعيل وضع المطور وهو محظور عليه
+            if kw.get('debug'):
                 cids = request.httprequest.cookies.get('cids')
                 try:
                     company_id = int(cids.split(',')[0]) if cids else request.env.company.id
@@ -93,7 +90,7 @@ class Home(Home):
                 ], limit=1)
 
                 if access_management:
-                    # إعادة التوجيه لتعطيل الـ Debug
+                    # إجباره على العودة للوضع العادي
                     return request.redirect('/web?debug=0')
 
         return super(Home, self).web_client(s_action=s_action, **kw)
